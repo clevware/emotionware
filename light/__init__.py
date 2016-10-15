11df14d2242895e00265ec38111f81dc8f32152c
chaos <497386856@qq.com>
2016-10-15 15:53:25 +0800

    test

---
+++ light/__init__.py
@@ -0,0 +1,49 @@
+import requests,json
+from uuid import uuid4
+from gpiozero import LED
+import time
+
+class Light():
+
+    id = str()
+    serverURL = str()
+    session = requests.session()
+    light = LED()
+
+    #inti whit a id
+    def __init__(self,id="",serverURL="http://cleverhome.mybluemix.net",pin=17):
+        if(len(id)>0):
+            self.id = id
+        else:
+            self.id=uuid4()
+        if(len(serverURL)>0):
+            self.serverURL = serverURL
+        if(pin!=0):
+            self.light = LED(pin)
+
+    # send a request to get light state
+    def getLightState(self,serverURL):
+
+        data={
+            'id':self.id,
+        }
+
+        res=self.session.post(
+            url=self.serverURL,
+            data=data,
+        )
+        res = json.loads(res)
+
+        if(res['value']>0):
+            self.light.on()
+        else:
+            self.light.off()
+
+
+if(__name__=="__main__"):
+
+    testone = Light()
+
+    while True:
+        testone.getLightState()
+        time.sleep(2)
