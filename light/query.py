df7cac8010d7ab572ac6a7509baded81d6e47204
chaos <497386856@qq.com>
2017-05-12 01:18:15 +0800

    fix gpio setup

--- light/query.py
+++ light/query.py
@@ -16,6 +16,13 @@ def getSpeakerData(device_json):
         if('type' in d and d['type']=="speaker"):
             return d['state']
 
+def getMessage():
+    res = requests.get("http://wzhere.cn:7036/get_message")
+    res_json = json.loads(res)
+    if(res_json['flag']==1):
+        return res_json['message']
+    else:
+        return ''
 
 if(__name__=="__main__"):
     res = getDeviceData()
