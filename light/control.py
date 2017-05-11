43d24b6a0ec6e9fd25c803bef24e56a36fecd21f
chaos <497386856@qq.com>
2017-05-12 01:10:24 +0800

    fix gpio setup

--- light/control.py
+++ light/control.py
@@ -16,8 +16,9 @@ def changeDutyTo(p,val):
 
 if(__name__=="__main__"):
     #initial pin
-    GPIO.setup(USING_PIN, GPIO.OUT)
     GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(USING_PIN, GPIO.OUT)
+
     p = GPIO.PWM(12, 50) # 将12号引脚初始化为PWM实例 ，频率为50Hz
     p.start(0) # 开始脉宽调制，参数范围为： (0.0 <= dc <= 100.0)
 
