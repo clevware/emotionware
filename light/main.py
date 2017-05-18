98d0fb460b870929c838cafcdbc2592024e14e1b
chaos <497386856@qq.com>
2017-05-18 20:16:30 +0800

    add main script

--- light/main.py
+++ light/main.py
@@ -1,28 +1,18 @@
 # coding=utf-8
-from query import getDeviceData,getBlubData,getSpeakerData,getMessage
-import time
+from query import getDeviceData,getMessage
+from control import changeDutyTo,getBlubData,espeak
 import RPi.GPIO as GPIO
-import os
-
-USING_PIN = 11
-
-def changeDutyTo(p,val):
-    if(val>=0 and val <=100):
-        p.ChangeDutyCycle(val)
-        print "Change Bulb-value to [%d]" % val
-
-def espeak(message):
-    os.system("espeak '%s" % message)
+import time
 
 
 if(__name__=="__main__"):
-    #initial pin
+    USING_PIN = 12
+
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(USING_PIN, GPIO.OUT)
 
-    p = GPIO.PWM(USING_PIN, 12) # 将12号引脚初始化为PWM实例 ，频率为50Hz
+    p = GPIO.PWM(USING_PIN, USING_PIN) # 将12号引脚初始化为PWM实例 ，频率为50Hz
     p.start(0) # 开始脉宽调制，参数范围为： (0.0 <= dc <= 100.0)
-
 
     while(True):
         time.sleep(1)
